541017f7780082bb6ef081e840680a369b15c974
Resmi Ramani <Resmi.RamaniAmma@triosstudent.com>
2020-06-22 23:51:06 -0400

    To parse JSON

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const app = express();
 const https = require("https");
+const { response } = require("express");
 
 app.get("/", function (req, res) {
   const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=kitchener,ca&appid=deeca45352ba2570bec75fb9812f6c52&units=metrics";
+    "https://api.openweathermap.org/data/2.5/weather?q=kitchener,ca&appid=deeca45352ba2570bec75fb9812f6c52&units=metric";
   https.get(url, function (response) {
     console.log(response);
+    response.on("data", function (data) {
+      const WeatherData = JSON.parse(data);
+      const temp = WeatherData.main.temp;
+      const description = WeatherData.weather[0].description;
+      console.log(description);
+    });
   });
+
   res.send("Server is up and  running");
 });
 
